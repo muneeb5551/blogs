e07ad714577a993a8318b7ea676ba2c2bd2109cf
muneeb5551 <muhammadmuneebsultan@gmail.com>
2024-10-28 13:05:56 -0700

    services section add

--- resources/views/layouts/masterlayout.blade.php
+++ resources/views/layouts/masterlayout.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
@@ -36,13 +37,69 @@
                 </ul>
             </div>
             <div class="navbar-buttons">
-                <div class=""><p class="login-title">Login</p></div>
+                <div class="">
+                    <p class="login-title">Login</p>
+                </div>
                 <div class="register-button pages-title">Register</div>
 
             </div>
         </div>
     </div>
+    @yield('content')
 
+    <div class="container-fluid footer-main">
+        <div class="container footer-sub">
+            <div class="footer-part">
+                <div class="footer-logo">
+                    <img src="https://quickpostpk.com/uploads/settings/202410221724378175.webp" class="img-fluid w-100"
+                        alt="">
+                </div>
+                <div class="">
+                    <p class="footer-para">Fastest platform with all courier service features. Help you start, run and
+                        grow your courier service.</p>
+                </div>
+            </div>
+            <div class="footer-part">
+                <h3 class="footer-h3">Available Services </h3>
+                <div class="">
+                    <a href="" class="footer-linklist">E-Commerce delivery</a><br>
+                    <a href="" class="footer-linklist">Pick & drop</a><br>
+                    <a href="" class="footer-linklist">Packing</a><br>
+                    <a href="" class="footer-linklist">Warehousing</a>
+                </div>
+            </div>
+            <div class="footer-part">
+                <h3 class="footer-h3">About</h3>
+                <div class="">
+                    <a href="" class="footer-linklist">FAQ</a><br>
+                    <a href="" class="footer-linklist">About us</a><br>
+                    <a href="" class="footer-linklist">Contact us</a><br>
+                    <a href="" class="footer-linklist">Privacy And Policy</a><br>
+                    <a href="" class="footer-linklist">Terms of use</a>
+                </div>
+            </div>
+            <div class="footer-part">
+                <h3 class="footer-h3">Contact Us</h3>
+                <div class="">
+                    <p class="footer-para">You can contact with us through following.</p>
+                </div>
+                <div class="icon-footer">
+                    <p class="text-light"><i class="bi bi-envelope-fill"></i></p>
+                    <a href="" class="footer-linklist">abc123@abc.com</a>
+                </div>
+                <div class="icon-footer">
+                    <p class="text-light"><i class="bi bi-telephone-fill"></i></p>
+                    <a href="" class="footer-linklist">123456789</a>
+
+                </div>
+                <div class="icon-footer">
+                    <p class="text-light"><i class="bi bi-geo-alt-fill"></i></p>
+                    <a href="" class="footer-linklist">Johar Town Phase 2 Lahore,pakistan</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
@@ -57,6 +114,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script>
+    <script src="{{ asset('script/script.js') }}"></script>
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,134 @@
 @extends('layouts.masterlayout')
+@section('content')
+    <header id="header" role="header">
+        <section id="title-section">
+
+        </section>
+    </header>
+
+    <main id="main" role="main">
+        <section id="image-section">
+            <div class="container-fluid p-0">
+
+                <article id="contain-image">
+
+                    <img src="https://picsum.photos/id/526/3000/2000.jpg" alt="The wonderful magic of image sliders"
+                        class="img" id="img1">
+                    <img src="https://picsum.photos/id/521/3000/2000.jpg" alt="The wonderful magic of image sliders"
+                        class="img" id="img2">
+                    <img src="https://picsum.photos/id/857/3000/2000.jpg" alt="The wonderful magic of image sliders"
+                        class="img" id="img3">
+                    <img src="https://picsum.photos/id/1084/3000/2000.jpg" alt="The wonderful magic of image sliders"
+                        class="img" id="img4">
+                    <img src="https://picsum.photos/id/524/3000/2000.jpg" alt="The wonderful magic of image sliders"
+                        class="img" id="img5">
+                    <img src="https://picsum.photos/id/64/3000/2000.jpg" alt="The wonderful magic of image sliders"
+                        class="img" id="img6">
+                    <img src="https://picsum.photos/id/435/3000/2000.jpg" alt="The wonderful magic of image sliders"
+                        class="img" id="img7">
+
+                    <div id="contain-button">
+                        <ion-icon name="radio-button-on" class="button" id="button1"></ion-icon>
+                        <ion-icon name="radio-button-on" class="button" id="button2"></ion-icon>
+                        <ion-icon name="radio-button-on" class="button" id="button3"></ion-icon>
+                        <ion-icon name="radio-button-on" class="button" id="button4"></ion-icon>
+                        <ion-icon name="radio-button-on" class="button" id="button5"></ion-icon>
+                        <ion-icon name="radio-button-on" class="button" id="button6"></ion-icon>
+                        <ion-icon name="radio-button-on" class="button" id="button7"></ion-icon>
+                    </div>
+
+                </article>
+
+            </div>
+        </section>
+    </main>
+
+    <footer id="footer" role="footer">
+        <section id="footer-section">
+            <div class=container>
+                <p id="signature">At Quick Post, we bring the flavors of your favorite restaurants right to your
+                    doorstep. From savory bites to sweet treats, enjoy a delicious meal without the wait. With quick
+                    delivery and exclusive deals, satisfying your hunger has never been easier.</p>
+            </div>
+        </section>
+    </footer>
+
+
+    <div class="container-fluid">
+        <div class="container services-sub">
+            <h1 class="services-h1">Our Services</h1>
+
+        </div>
+        <div class="container p-0 scard-main">
+            <div class="services-card">  
+            <div class="services-card-sub">
+                <h1 class="scard-logo"><i class="bi bi-amazon"></i></h1>
+            </div>
+            <div class="scard-text">
+                <h1 class="sct-h1">Online Delivery</h1>
+                <div class="">
+                    <p class="sct-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora alias sapiente
+                        necessitatibus sequi voluptas excepturi, quaerat repellat repellendus quas corporis,
+                        placeat, dolor magni reiciendis deserunt corrupti.</p>
+                    </div>
+                </div>
+                <div class="arrow-circle"><p class="arrow-size"><i class="bi bi-caret-right-fill"></i></p></div>
+            </div>
+            
+            <div class="services-card">  
+                <div class="services-card-sub">
+                    <h1 class="scard-logo"><i class="bi bi-truck"></i></h1>
+                </div>
+                <div class="scard-text">
+                    <h1 class="sct-h1">Pick & Drop</h1>
+                    <div class="">
+                        <p class="sct-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora alias sapiente
+                            necessitatibus sequi voluptas excepturi, quaerat repellat repellendus quas corporis,
+                            placeat, dolor magni reiciendis deserunt corrupti.</p>
+                        </div>
+                    </div>
+                    <div class="arrow-circle"><p class="arrow-size"><i class="bi bi-caret-right-fill"></i></p></div>
+                </div>
+            
+                <div class="services-card">  
+                    <div class="services-card-sub">
+                        <h1 class="scard-logo"><i class="bi bi-boxes"></i></h1>
+                    </div>
+                    <div class="scard-text">
+                        <h1 class="sct-h1">Packaging</h1>
+                        <div class="">
+                            <p class="sct-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora alias sapiente
+                                necessitatibus sequi voluptas excepturi, quaerat repellat repellendus quas corporis,
+                                placeat, dolor magni reiciendis deserunt corrupti.</p>
+                            </div>
+                        </div>
+                        <div class="arrow-circle"><p class="arrow-size"><i class="bi bi-caret-right-fill"></i></p></div>
+                    </div>
+            
+                    <div class="services-card">  
+                        <div class="services-card-sub">
+                            <h1 class="scard-logo"><i class="bi bi-houses"></i></h1>
+                        </div>
+                        <div class="scard-text">
+                            <h1 class="sct-h1">Warehousing</h1>
+                            <div class="">
+                                <p class="sct-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora alias sapiente
+                                    necessitatibus sequi voluptas excepturi, quaerat repellat repellendus quas corporis,
+                                    placeat, dolor magni reiciendis deserunt corrupti.</p>
+                                </div>
+                            </div>
+                            <div class="arrow-circle"><p class="arrow-size"><i class="bi bi-caret-right-fill"></i></p></div>
+                        </div>
+        </div>
+
+
+
+
+    </div>
+
+    <div class="container-fluid traking-main">
+        <div class="container traking-sub">
+            
+        </div>
+    </div>
+@endsection
